4fd126c187d21f669b14f4ff87c149851c05d124
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-01-18 17:44:51 +0100

    commit

--- src/main/resources/db.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO address (id, auteur,creation, content) VALUES (1, "Maria",CURRENT_TIMESTAMP(), '57 boulevard demorieux');
-
-INSERT INTO address (id, auteur,creation, content) VALUES (2, "Fariss",CURRENT_TIMESTAMP(), '51 allee du gamay, 34080
-montpellier');  

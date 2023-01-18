7daf4dcae5fbf2c54b931caeaa6a402756fbfc2f
Maria Fariss <mariafariss0202@gmail.com>
2023-01-18 11:18:11 +0100

    commit

---
+++ src/main/java/com/ensim/TP3/model/Address.java
@@ -0,0 +1,28 @@
+package com.ensim.TP3.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date creation;
+    private String content;
+	public Date getCreation() {
+		return creation;
+	}
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}

d7998a6792639dddedca4d234288cc13351ccd39
Maria fariss <mariafariss0202@gmail.com>
2023-01-23 16:47:19 +0100

    etape3v1

--- src/main/java/com/ensim/TP3/controller/MeteoController.java
+++ src/main/java/com/ensim/TP3/controller/MeteoController.java
@@ -1,11 +1,13 @@
 package com.ensim.TP3.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import com.ensim.TP3.model.AddressRepository;
 
@@ -13,9 +15,13 @@ import com.ensim.TP3.model.AddressRepository;
 public class MeteoController {
 	 @Autowired
 	    AddressRepository addressRepository;
+	 	RestTemplate restemplate = new RestTemplate();
+	 
 
 	    @PostMapping("/meteo")
 	    public String showAddresses(@RequestParam(name="address") String nameGET, Model model) {
+	    	ResponseEntity<MyResponse> response =  restemplate.getForEntity("https://api-adresse.data.gouv.fr/search/?q=+nameGET", MyResponse.class);
+	        MyResponse myResponse = response.getBody();
 	    	model.addAttribute("nomTemplate", nameGET);
 	        return "meteo";
 	    }
